ece5a56787a5afff5fee13bd6d2fdc7d9cf4b98e
Gwen <gfdipert@gmail.com>
2016-01-04 13:26:05 -0500

    Fixed client list view

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.client_list, name='client_list'),
     url(r'^client/(?P<pk>[0-9]+)/$', views.client_detail, name='client_detail'),
     url(r'^client/new/$', views.client_new, name='client_new')
 ]
