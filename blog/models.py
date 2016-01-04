26c6adf4145c88bdc352480104ff3e7c208230df
Gwen <gfdipert@gmail.com>
2016-01-04 10:34:33 -0500

    Created Client Model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
+from django.conf import settings
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
@@ -18,4 +20,45 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-# Create your models here.
+
+class Client(models.Model):
+    GSTATUS = (
+        (0,'New'),
+        (1,'Guidebuilding'),
+        (2,'Guidebuild Sufficient'),
+        (3,'Complete'),
+    )
+    APPSTATUS = (
+        (0,'Pending Assets'),
+        (1,'Mockups Sent'),
+        (2,'Screenshots Sent'),
+        (3,'Apple Submitted'),
+        (4,'Apple Published'),
+    )
+    PSTATUS = (
+        (0,'Not Submitted'),
+        (1,'Submitted'),
+        (2,'Play Published'),
+    )
+    BSTATUS = (
+        (0,'Not Submitted'),
+        (1,'Submitted'),
+        (2,'Blackberry Published'),
+    )
+    ASTATUS = (
+        (0,'Not Submitted'),
+        (1,'Submitted'),
+        (2,'Amazon Published'),
+    )
+    name = models.CharField(max_length=45, unique=True, verbose_name=_('Name'))
+    gstatus = models.IntegerField(choices=GSTATUS, default=0, verbose_name=_('Guidebuilding Status'))
+    appstatus = models.IntegerField(choices=APPSTATUS, default=0, verbose_name=_('Apple Store Status'))
+    pstatus = models.IntegerField(choices=PSTATUS, default=0, verbose_name=_('Google Play Status'))
+    bstatus = models.IntegerField(choices=BSTATUS, default=0, verbose_name=_('Blackberry Status'))
+    astatus = models.IntegerField(choices=ASTATUS, default=0, verbose_name=_('Amazon Status'))
+    intro = models.DateField(null=True, blank=True, verbose_name=_('Intro Email Sent'))
+    appsub = models.DateField(null=True, blank=True, verbose_name=_('Apple Submitted'))
+    apppub = models.DateField(null=True, blank=True, verbose_name=_('Apple Published'))
+    guides = models.TextField(default='Guide Names')
+    def __unicode__(self):
+        return self.name
