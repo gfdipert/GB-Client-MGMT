76d3cb324c5d761d50b871cb7bf5de80a8a527a5
Gwen <gfdipert@gmail.com>
2016-01-04 13:21:44 -0500

    Fixed client view and detail pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^client/(?P<cl>[0-9]+)/$', views.client_detail, name='client_detail'),
+    url(r'^client/(?P<pk>[0-9]+)/$', views.client_detail, name='client_detail'),
     url(r'^client/new/$', views.client_new, name='client_new')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from .models import Client
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from .forms import ClientForm
 
 # Create your views here.
 
@@ -10,6 +11,10 @@ def post_list(request):
     clients = Client.objects.filter(intro__lte=timezone.now()).order_by('intro')
     return render(request, 'blog/post_list.html', {'clients':clients})
 
-def client_detail(request, cl):
-    post = get_object_or_404(Post, cl=cl)
+def client_detail(request, pk):
+    client = get_object_or_404(Client, pk=pk)
     return render(request, 'blog/client_detail.html', {'client': client})
+
+def client_new(request):
+	form = ClientForm()
+	return render(request, 'blog/client_edit.html', {'form':form})
