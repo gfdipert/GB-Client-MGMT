ca218aeec7e58e735454a17c1ae670c793dcef8a
Gwen <gfdipert@gmail.com>
2016-01-08 13:03:17 -0500

    Reformatted imports to match PEP-8 standards

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from .models import Post
 from .models import Client
-from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
 from .forms import ClientForm
-from django.shortcuts import redirect
 
 # Create your views here.
 
